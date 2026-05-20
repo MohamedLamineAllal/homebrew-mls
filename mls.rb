8146bdf97792406046d4b1cf51fa4a444df5ebd7
Mohamed Lamine Allal <allalmohamedlamine@gmail.com>
2026-05-20 16:11:24 +0800

    Initial commit

---
+++ mls.rb
@@ -0,0 +1,18 @@
+class Mls < Formula
+  desc "High-performance storage cleanup tool for macOS"
+  homepage "https://github.com/MohamedLamineAllal/MacOSLeanStorage"
+  url "https://github.com/MohamedLamineAllal/MacOSLeanStorage/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "REPLACE_WITH_SHA256_HASH" 
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", "mls", "main.go"
+    bin.install "mls"
+  end
+
+  test do
+    system "#{bin}/mls", "--version"
+  end
+end
